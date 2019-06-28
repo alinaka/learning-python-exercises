771992dc304ebddccfd210e6cef1a6e344512fee
alinaka <alinakago@gmail.com>
2019-06-28 17:03:37 +0300

    adds diagonal_traverse

--- leetcode/array_and_string/2d-array/diagonal_traverse/diagonal_traverse.py
+++ leetcode/array_and_string/2d-array/diagonal_traverse/diagonal_traverse.py
@@ -9,3 +9,31 @@ Input:
 ]
 Output: [1, 2, 4, 7, 5, 3, 6, 8, 9]
 """
+from typing import List
+
+
+class Solution:
+    def findDiagonalOrder(self, matrix: List[List[int]]) -> List[int]:
+        if not matrix or not matrix[0]:
+            return []
+        moveDown = 0
+        result = []
+        m = len(matrix)
+        n = len(matrix[0])
+        for x in range(n + m - 1):
+            if moveDown % 2:
+                for j in range(min(x, n - 1), max(x - m, -1), -1):
+                    result.append(matrix[x-j][j])
+            else:
+                for i in range(min(x, m - 1), max(x - n, -1), -1):
+                    result.append(matrix[i][x-i])
+            moveDown += 1
+        return result
+
+
+if __name__ == "__main__":
+    with open("input.txt") as file:
+        nums = eval(file.read().strip())
+
+    s = Solution().findDiagonalOrder(nums)
+    print(s)
